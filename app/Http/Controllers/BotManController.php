a78c6ccc9cdb656fb18c1a6205283efc5f3e805d
Hardik Savani <itsolutionstuff@gmail.com>
2019-07-06 10:23:02 +0530

    setp

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use BotMan\BotMan\BotMan;
+use Illuminate\Http\Request;
+use BotMan\BotMan\Messages\Incoming\Answer;
+
+class BotManController extends Controller
+{
+    /**
+     * Place your BotMan logic here.
+     */
+    public function handle()
+    {
+        $botman = app('botman');
+
+        $botman->hears('{message}', function($botman, $message) {
+
+            if ($message == 'hi') {
+                $this->askName($botman);
+            }else{
+                $botman->reply("write 'hi' for testing...");
+            }
+
+        });
+
+        $botman->listen();
+    }
+
+    /**
+     * Place your BotMan logic here.
+     */
+    public function askName($botman)
+    {
+        $botman->ask('Hello! What is your Name?', function(Answer $answer) {
+            $name = $answer->getText();
+
+            $this->say('Nice to meet you '.$name);
+        });
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>How to install Botman Chatbot in Laravel 5? - ItSolutionStuff.com</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+        </style>
+    </head>
+    <body>
+    </body>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/assets/css/chat.min.css">
+    <script>
+    var botmanWidget = {
+        aboutText: 'ssdsd',
+        introMessage: "✋ Hi! I'm form ItSolutionStuff.com"
+    };
+    </script>
+    <script src='https://cdn.jsdelivr.net/npm/botman-web-widget@0/build/js/widget.js'></script>
+    
+</html>
